984a4cb9f3d50bbd9c5e7d0c968343c7cf570c79
benjaminuj <chosj1526@gmail.com>
2024-07-11 01:27:11 +0900

    feat: UserController 테스트 코드 추가

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'meme@naver.com', 'meme', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'mymy@naver.com', 'mymy', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
